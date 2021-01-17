0c7fc990b7c76573a16eeebf9851fc0c0b239df4
arielycliu <ariel.yc.liu@gmail.com>
2021-01-17 05:00:01 -0500

    documents not working

--- src/pages/icons/Icons.js
+++ src/pages/icons/Icons.js
@@ -21,6 +21,78 @@ import PageTitle from "../../components/PageTitle/PageTitle";
 // icons sets
 import "font-awesome/css/font-awesome.min.css";
 
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Sector,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+// components
+import Widget from "../../components/Widget/Widget";
+import ApexLineChart from "./components/ApexLineChart";
+import ApexHeatmap from "./components/ApexHeatmap";
+
+const lineChartData = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const pieChartData = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+];
+
+
 export default function IconsPage() {
   const [anchorEl, setAnchorEl] = React.useState(null);
 
@@ -80,7 +152,16 @@ export default function IconsPage() {
         {activeTabId === 0 && (
           <div>
             <Grid container spacing={2} className="icon-list">
-              
+            <Grid item xs={12} md={6}>
+          <Widget title="Apex Line Chart" upperTitle noBodyPadding>
+            <ApexLineChart />
+          </Widget>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Widget title="Apex Heatmap" upperTitle noBodyPadding>
+            <ApexHeatmap />
+          </Widget>
+        </Grid>
             </Grid>
           </div>
         )}
