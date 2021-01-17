a9e74802724b972406a0b4aed37bf5a09d580206
mkim0302 <mkim0302@students.cs.ubc.ca>
2021-01-16 22:54:22 -0800

    added details to patient list on dashboard.js, edited patient details page to include 4 boxes of patient info

--- src/components/Sidebar/SidebarView.js
+++ src/components/Sidebar/SidebarView.js
@@ -13,7 +13,7 @@ import {
   QuestionAnswer as SupportIcon,
   LibraryBooks as LibraryIcon,
   HelpOutline as FAQIcon,
-  ArrowBack as ArrowBackIcon,
+  ArrowBack as ArrowBackIcon, Face, AirlineSeatFlat, PeopleOutline, ListAlt, HdrWeak, DirectionsWalk,
 } from "@material-ui/icons";
 import classNames from 'classnames';
 
@@ -22,30 +22,24 @@ import Dot from './components/Dot';
 
 const structure = [
   { id: 0, label: 'Dashboard', link: '/app/dashboard', icon: <HomeIcon /> },
-  { id: 1, label: 'Typography', link: '/app/typography', icon: <TypographyIcon /> },
-  { id: 2, label: 'Tables', link: '/app/tables', icon: <TableIcon /> },
-  { id: 3, label: 'Notifications', link: '/app/notifications', icon: <NotificationsIcon />},
+  { id: 1, label: 'Patient Details', link: '/app/typography', icon: <Face /> },
+  { id: 2, label: 'Admission', link: '/app/tables', icon: <AirlineSeatFlat /> },
+  { id: 3, label: 'Orders', link: '/app/notifications', icon: <NotificationsIcon />},
   {
     id: 4,
-    label: 'UI Elements',
+    label: 'Documentation',
     link: '/app/ui',
     icon: <UIElementsIcon />,
-    children: [
-      { label: 'Icons', link: '/app/ui/icons' },
-      { label: 'Charts', link: '/app/ui/charts' },
-      { label: 'Maps', link: '/app/ui/maps' },
-    ],
   },
-  { id: 5, type: 'divider' },
-  { id: 6, type: 'title', label: 'HELP' },
-  { id: 7, label: 'Library', link: '', icon: <LibraryIcon /> },
-  { id: 8, label: 'Support', link: '', icon: <SupportIcon /> },
-  { id: 9, label: 'FAQ', link: '', icon: <FAQIcon />},
-  { id: 10, type: 'divider' },
-  { id: 11, type: 'title', label: 'PROJECTS' },
-  { id: 12, label: 'My recent', link: '', icon: <Dot size="small" color="secondary" /> },
-  { id: 13, label: 'Starred', link: '', icon: <Dot size="small" color="primary" /> },
-  { id: 14, label: 'Background', link: '', icon: <Dot size="small" color="secondary" /> },
+  { id: 5, label: 'Referral', link: '/app/referral', icon: <PeopleOutline />},
+  { id: 6, label: 'Results', link: '/app/results', icon: <ListAlt />},
+  { id: 7, label: 'Medication', link: '/app/medication', icon: <HdrWeak />},
+  { id: 8, label: 'Discharge', link: '/app/discharge', icon: <DirectionsWalk />},
+  { id: 9, type: 'divider' },
+  { id: 10, type: "title", label: "HELP" },
+  { id: 11, label: 'Resources', link: '', icon: <LibraryIcon /> },
+  { id: 12, label: 'Support', link: '', icon: <SupportIcon /> },
+  { id: 13, type: 'divider' },
 ];
 
 const SidebarView = ({ classes, theme, toggleSidebar, isSidebarOpened, isPermanent, location }) => {
--- src/pages/typography/Typography.js
+++ src/pages/typography/Typography.js
@@ -8,40 +8,81 @@ import useStyles from "./styles";
 import PageTitle from "../../components/PageTitle";
 import Widget from "../../components/Widget";
 import { Typography } from "../../components/Wrappers";
+import { black } from "color-name";
 
 export default function TypographyPage() {
   var classes = useStyles();
 
   return (
     <>
-      <PageTitle title="Typography" />
+      <PageTitle title={"Patient in Focus: Monster, Cookie"}/>
       <Grid container spacing={4}>
         <Grid item xs={12} md={6}>
-          <Widget title="Headings" disableWidgetMenu>
+          <Widget title="Contact Info" disableWidgetMenu>
             <div className={classes.dashedBorder}>
-              <Typography variant="h1" className={classes.text}>
-                h1. Heading
+              <Typography>
+                <Typography color="primary">First Name:</Typography>Cap'n Horatio Magellan
               </Typography>
-              <Typography variant="h2" className={classes.text}>
-                h2. Heading
+              <Typography>
+                <Typography color="primary">Last Name:</Typography>Crunch
               </Typography>
-              <Typography variant="h3" className={classes.text}>
-                h3. Heading
+              <Typography>
+                <Typography color="primary">Preferred Name:</Typography>Cookie Monster
               </Typography>
-              <Typography variant="h4" className={classes.text}>
-                h4. Heading
+              <Typography>
+                <Typography color="primary">Sex:</Typography>Male
               </Typography>
-              <Typography variant="h5" className={classes.text}>
-                h5. Heading
+              <Typography>
+                <Typography color="primary">Age:</Typography>47
               </Typography>
-              <Typography variant="h6">h6. Heading</Typography>
+              <Typography>
+                <Typography color="primary">Date of Birth:</Typography>Nov. 2, 1973
+              </Typography>
+              <Typography>
+                <Typography color="primary">Primary Residence:</Typography>123 Sesame Street
+              </Typography>
+              <Typography>
+                <Typography color="primary">Primary Number:</Typography>123-4567
+              </Typography>
+              <Typography>
+                <Typography color="primary">Emergency Contact Name:</Typography>Elmo
+              </Typography>
+              <Typography>
+                <Typography color="primary">Emergency Contact Primary Number:</Typography>333-3333
+              </Typography>
             </div>
           </Widget>
         </Grid>
         <Grid item xs={12} md={6}>
-          <Widget title="Typography Colors" disableWidgetMenu>
+          <Widget title="History" disableWidgetMenu>
             <div className={classes.dashedBorder}>
-              <Typography variant="h1" color="primary" className={classes.text}>
+              <Typography>
+                <Typography color="primary">Height:</Typography>38"
+              </Typography>
+              <Typography>
+                <Typography color="primary">Current Body Weight:</Typography>40 kg
+              </Typography>
+              <Typography>
+                <Typography color="primary">Blood Type:</Typography>A-
+              </Typography>
+              <Typography>
+                <Typography color="primary">Diet Restriction:</Typography>No fruits or vegetables
+              </Typography>
+              <Typography>
+                <Typography color="primary">Religion:</Typography>Cookie Cult
+              </Typography>
+              <Typography>
+                <Typography color="primary">Past Medical History:</Typography>Binge eating disorder, diabetes mellitus
+                type II
+              </Typography>
+              <Typography>
+                <Typography color="primary">Surgical History:</Typography>Surgical History: Gastric bypass (est. July
+                2010)
+              </Typography>
+              <Typography>
+                <Typography color="primary">Current Medications:</Typography>Metformin 850mg OD with meal
+              </Typography>
+              {/*              <Typography variant="h1" color="primary" className={classes.text}>
                 h1. Heading
               </Typography>
               <Typography variant="h2" color="success" className={classes.text}>
@@ -67,14 +108,23 @@ export default function TypographyPage() {
               </Typography>
               <Typography variant="h6" color="info">
                 h6. Heading
-              </Typography>
+              </Typography>*/}
             </div>
           </Widget>
         </Grid>
         <Grid item xs={12} md={6}>
-          <Widget title="Basic Text Settings" disableWidgetMenu>
+          <Widget title="Care Team" disableWidgetMenu>
             <div className={classes.dashedBorder}>
-              <Typography className={classes.text}>Basic text</Typography>
+              <Typography>
+                <Typography color="primary">General Physician:</Typography>Dr. Theodor Seuss
+              </Typography>
+              <Typography>
+                <Typography color="primary">Most Responsible Provider:</Typography>Dr. Derek Shepherd
+              </Typography>
+              <Typography>
+                <Typography color="primary">Primary RN:</Typography>Eli Lloyd
+              </Typography>
+              {/*             <Typography className={classes.text}>Basic text</Typography>
               <Typography className={classes.text} weight="light">
                 Basic light text
               </Typography>
@@ -95,14 +145,23 @@ export default function TypographyPage() {
               </Typography>
               <Typography>
                 <i>Basic Cursive Text</i>
-              </Typography>
+              </Typography>*/}
             </div>
           </Widget>
         </Grid>
         <Grid item xs={12} md={6}>
-          <Widget title="Text Size" disableWidgetMenu>
+          <Widget title="Advisory" disableWidgetMenu>
             <div className={classes.dashedBorder}>
-              <Typography className={classes.text} size="sm">
+              <Typography>
+                <Typography color="primary">Allergies:</Typography>Fruits and vegetables
+              </Typography>
+              <Typography>
+                <Typography color="primary">Alerts:</Typography>Flight risk
+              </Typography>
+              <Typography>
+                <Typography color="primary">Code Status:</Typography>DNR M3
+              </Typography>
+ {/*             <Typography className={classes.text} size="sm">
                 Heading Typography SM Font Size
               </Typography>
               <Typography className={classes.text}>
@@ -116,7 +175,7 @@ export default function TypographyPage() {
               </Typography>
               <Typography className={classes.text} size="xxl">
                 Heading Typography XXL Font Size
-              </Typography>
+              </Typography>*/}
             </div>
           </Widget>
         </Grid>
